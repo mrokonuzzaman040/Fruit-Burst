3134fc3dc02fcca243230dc5857e3a91542454d8
mrokonuzzaman040 <19201103040@cse.bubt.edu.bd>
2023-07-26 21:36:18 +0600

    Working Fine

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        animation: {
+          'spin-slow': 'spin 3s linear infinite',
+        }
+      }
+    },
   },
   plugins: [],
 }
